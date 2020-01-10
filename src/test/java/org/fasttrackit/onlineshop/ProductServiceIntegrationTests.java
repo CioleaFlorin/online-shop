da11c78f5de54b4242c23f93cecbf00880bbbf78
Florin <ciolea.florin.99@gmail.com>
2020-01-10 18:49:40 +0200

    finished test in CartServiceIntegTest

--- src/test/java/org/fasttrackit/onlineshop/ProductServiceIntegrationTests.java
+++ src/test/java/org/fasttrackit/onlineshop/ProductServiceIntegrationTests.java
@@ -3,6 +3,7 @@ package org.fasttrackit.onlineshop;
 import org.fasttrackit.onlineshop.domain.Product;
 import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.service.ProductService;
+import org.fasttrackit.onlineshop.steps.ProductSteps;
 import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
 
 import org.junit.Test;
@@ -24,6 +25,9 @@ public class ProductServiceIntegrationTests {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private ProductSteps productSteps;
+
 
     @Test(expected = TransactionSystemException.class)
     public void testCreateProduct_whenInvalidRequest_thenThrowException() {
@@ -36,7 +40,7 @@ public class ProductServiceIntegrationTests {
 
     @Test
     public void testGetProduct_whenExistingProduct_thenReturnProduct() {
-        Product createdProduct = createProduct();
+        Product createdProduct = productSteps.createProduct();
         Product product=productService.getProduct(createdProduct.getId());
 
         assertThat(product, notNullValue());
@@ -52,7 +56,7 @@ public class ProductServiceIntegrationTests {
 
     @Test
     public void testCreateProduct_whenValidRequest_thenProductIsSaved() {
-        createProduct();
+        productSteps.createProduct();
     }
 
 
@@ -64,7 +68,7 @@ public class ProductServiceIntegrationTests {
     }
     @Test
     public void testUpdateProduct_whenValidRequest_thenReturnUpdatedProduct() {
-        Product createdProduct = createProduct();
+        Product createdProduct = productSteps.createProduct();
 
         SaveProductRequest request=new SaveProductRequest();
         request.setName(createdProduct.getName()+" updated");
@@ -84,30 +88,14 @@ public class ProductServiceIntegrationTests {
 
     @Test(expected = ResourceNotFoundException.class)
     public void testDeleteProduct_whenExistingProduct_thenProductIsDeleted(){
-        Product product = createProduct();
+        Product product = productSteps.createProduct();
 
         productService.deleteProduct(product.getId());
 
         productService.getProduct(product.getId());
     }
 
-    private Product createProduct() {
-        SaveProductRequest request = new SaveProductRequest();
-        request.setName("Banana");
-        request.setPrice(5.0);
-        request.setQuantity(100);
-        request.setDescription("Healthy food");
 
-        Product createdProduct = productService.createProduct(request);
-        assertThat(createdProduct, notNullValue());
-        assertThat(createdProduct.getId(), notNullValue());
-        assertThat(createdProduct.getId(), greaterThan(0L));
-        assertThat(createdProduct.getName(), is(request.getName()));
-        assertThat(createdProduct.getDescription(), is(request.getDescription()));
-        assertThat(createdProduct.getPrice(), is(request.getPrice()));
-        assertThat(createdProduct.getQuantity(), is(request.getQuantity()));
-        return createdProduct;
-    }
 
 }
 
